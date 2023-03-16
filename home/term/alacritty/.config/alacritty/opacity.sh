c8216c74e478dce40a0eed47f7c7087b8f4fa050
Amlanjyoti Lahkar <amlanjyotilahkar@keemail.me>
2023-03-16 15:51:25 +0530

    feat(alacritty): toggle opacity
    Little script for toggling alacritty window opacity.
    origin: https://trunc8.github.io/2021/08/08/tut-toggle-alacritty-opacity

---
+++ home/term/alacritty/.config/alacritty/opacity.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# change alacritty window opacity
+
+CONFIG_PATH="${XDG_CONFIG_HOME}/alacritty/alacritty.yml"
+[ ! -e "$CONFIG_PATH" ] && exit 1
+
+opacity=$(awk '$1 == "opacity:" {print $2; exit}' "$CONFIG_PATH")
+case $opacity in
+  1)
+    toggle_opacity=0.86
+    ;;
+  *)
+    toggle_opacity=1
+    ;;
+esac
+
+sed -i -- "s/opacity: $opacity/opacity: $toggle_opacity/" "$CONFIG_PATH"
