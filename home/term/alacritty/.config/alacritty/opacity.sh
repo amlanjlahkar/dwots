a69a606b8faf0806a362a55b030f719bb788a807
amlanjlahkar <amlanjyotilahkar@keemail.me>
2024-01-01 13:17:15 +0530

    misc(term): convert alacritty config
    Migrate alacritty's configuration from YAML to TOML format

--- home/term/alacritty/.config/alacritty/opacity.sh
+++ home/term/alacritty/.config/alacritty/opacity.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-# change alacritty window opacity
-
-CONFIG_PATH="${XDG_CONFIG_HOME}/alacritty/alacritty.yml"
+CONFIG_PATH="${XDG_CONFIG_HOME}/alacritty/alacritty.toml"
 [ ! -e "$CONFIG_PATH" ] && exit 1
 
-opacity=$(awk '$1 == "opacity:" {print $2; exit}' "$CONFIG_PATH")
+opacity=$(awk -F'= ' '/opacity/ {print $2; exit}' "$CONFIG_PATH")
 case $opacity in
-  1)
+  1.0)
     toggle_opacity=0.94
     ;;
   *)
-    toggle_opacity=1
+    toggle_opacity=1.0
     ;;
 esac
 
-sed -i -- "s/opacity: $opacity/opacity: $toggle_opacity/" "$CONFIG_PATH"
+sed -i -- "s/opacity = $opacity/opacity = $toggle_opacity/" "$CONFIG_PATH"
