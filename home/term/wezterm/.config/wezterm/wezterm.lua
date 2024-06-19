268e58e77e004bbdb6650e8fcf082b6958a8b62d
amlanjlahkar <amlanjlahkar@protonmail.com>
2024-06-19 18:59:03 +0530

    update wezterm pkg

--- home/term/wezterm/.config/wezterm/wezterm.lua
+++ home/term/wezterm/.config/wezterm/wezterm.lua
@@ -14,10 +14,10 @@ conf.default_prog = { "/opt/homebrew/bin/bash", "-l", "-c", "tmux_bind_fzf" }
 conf.audible_bell = "Disabled"
 
 conf.window_decorations = "RESIZE"
--- term.on("gui-startup", function()
---     local _, _, window = term.mux.spawn_window({})
---     window:gui_window():maximize()
--- end)
+term.on("gui-startup", function()
+    local _, _, window = term.mux.spawn_window({})
+    window:gui_window():maximize()
+end)
 
 -- Appearance
 conf.enable_tab_bar = false
@@ -29,10 +29,19 @@ conf.window_padding = {
     bottom = "0.5cell",
 }
 
--- conf.window_background_opacity = 0.9
--- conf.macos_window_background_blur = 20
 conf.animation_fps = 1
 conf.default_cursor_style = "SteadyBlock"
+-- conf.window_background_opacity = 0.9
+-- conf.macos_window_background_blur = 0
+
+-- conf.background = {
+--     {
+--         source = { File = "" },
+--         hsb = { brightness = 0.05, saturation = 0.8 },
+--         vertical_align = "Bottom",
+--         vertical_offset = "4cell",
+--     },
+-- }
 
 -- Colors
 conf.color_scheme_dirs = { "$HOME/.config/wezterm/colors/" }
@@ -41,7 +50,8 @@ conf.color_scheme = "boo"
 -- Font
 conf.font = term.font_with_fallback({
     "Serious Sans",
-    { family = "Serious Sans Nerd Font Mono", scale = 1 },
+    { family = "Symbols Nerd Font", scale = 0.85 },
+    { family = "Apple Symbols", scale = 1 },
     { family = "Apple Color Emoji", assume_emoji_presentation = true },
 })
 
@@ -49,27 +59,37 @@ conf.font_rules = {
     {
         intensity = "Normal",
         italic = true,
-        font = term.font("Serious Sans Italic", { style = "Normal" }),
+        font = term.font({
+            family = "Serious Sans Italic",
+            style = "Normal",
+        }),
     },
 
     {
         intensity = "Bold",
         italic = false,
-        font = term.font("Serious Sans Bold", { style = "Normal" }),
+        font = term.font({
+            family = "Serious Sans Bold",
+            style = "Normal",
+        }),
     },
 
     {
         intensity = "Bold",
         italic = true,
-        font = term.font("Serious Sans Bold Italic", { style = "Normal" }),
+        font = term.font({
+            family = "Serious Sans Bold Italic",
+            style = "Normal",
+        }),
     },
 }
 
 conf.font_size = 18
-conf.line_height = 1
+conf.line_height = 1.1
 
 conf.freetype_load_target = "Light"
 conf.freetype_render_target = "HorizontalLcd"
+conf.freetype_load_flags = "NO_HINTING"
 
 -- Keybinds
 conf.disable_default_key_bindings = true
